f4c5effe8e811568e7696b45f9a7bada4f43f415
orezybsk <orezybsk@sample.co.jp>
2020-02-16 10:15:05 +0000

    build.gradle 内でコメントアウトしていた DB, Redis 系のモジュールのコメント状態を解除し、Web アプリ起動時にエラーが出ないように調整しました

--- sample-webapp/src/main/java/orezybsk/aws/sample/webapp/Application.java
+++ sample-webapp/src/main/java/orezybsk/aws/sample/webapp/Application.java
@@ -2,14 +2,20 @@ package orezybsk.aws.sample.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * メインクラス
  */
-@SpringBootApplication
-//@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = {JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class Application {
 
+    /**
+     * メインメソッド
+     *
+     * @param args 起動時オプション
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
---
+++ sample-webapp/src/main/java/orezybsk/aws/sample/webapp/config/ApplicationConfig.java
@@ -0,0 +1,71 @@
+package orezybsk.aws.sample.webapp.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+import javax.sql.DataSource;
+
+/**
+ * アプリケーション用 Configuration クラス
+ */
+@Configuration
+public class ApplicationConfig {
+
+    private final MessageSource messageSource;
+
+    private final MBeanExporter mbeanExporter;
+
+    /**
+     * @param messageSource {@link MessageSource} bean
+     * @param mbeanExporter {@link MBeanExporter} bean
+     */
+    public ApplicationConfig(MessageSource messageSource
+            , @Autowired(required = false) MBeanExporter mbeanExporter) {
+        this.messageSource = messageSource;
+        this.mbeanExporter = mbeanExporter;
+    }
+
+    /**
+     * @return ???
+     */
+    @Bean
+    public Java8TimeDialect java8TimeDialect() {
+        return new Java8TimeDialect();
+    }
+
+    /**
+     * javax.validation や Hibernate Validator のメッセージを ValidationMessages.properties ではなく
+     * messages.properties に記述できるようにするために定義している
+     *
+     * @return new {@link LocalValidatorFactoryBean}
+     */
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setValidationMessageSource(this.messageSource);
+        return localValidatorFactoryBean;
+    }
+
+    /**
+     * @return HikariCP の DataSource オブジェクト
+     */
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource() {
+        if (mbeanExporter != null) {
+            mbeanExporter.addExcludedBean("dataSource");
+        }
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+}
